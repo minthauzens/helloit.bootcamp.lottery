01a481385b0ea4c556c235e24299bb748944bd69
Valters <valters@echoWork>
2020-02-04 23:08:45 +0200

    created ParticipantRegisterDto with basic validation (annotations)

--- src/main/java/lv/helloit/bootcamp/lottery/participant/ParticipantRegisterDto.java
+++ src/main/java/lv/helloit/bootcamp/lottery/participant/ParticipantRegisterDto.java
@@ -1,11 +1,12 @@
 package lv.helloit.bootcamp.lottery.participant;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 
 @Data
 @AllArgsConstructor
@@ -13,8 +14,15 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class ParticipantRegisterDto {
     @NotNull(message = "Lottery id must be provided")
-    private Long id;
+    @JsonProperty("id")
+    private Long lottery_id;
+    @Email(message = "must provide valid email")
+    @Size(min = 5, max=99, message = "email name length can't exceed 99")
     private String email;
+    @NotNull
+    @Min(value = 21, message = "Only people over 21, can participate in lotteries")
+    @Max(value = 100, message = "People over 100 should have different priorities")
     private int age;
+    @Digits(integer =16,fraction = 0, message = "The code must be consisting of 16 digits")
     private String code;
 }
